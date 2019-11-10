6df3880d02cc60b0aa593b8f58d4ac8a4a5eeb6c
Nick Daniele <ngdaniele@gmail.com>
2019-11-10 07:00:55 -0800

    Basic webpack config completed.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+(function starter() {
+  const root = document.getElementById('root');
+
+  const greeting = document.createElement('p');
+
+  greeting.innerText = 'Hello World!';
+
+  root.append(greeting);
+})();
