ea606bc1a1abb7c03bf393a815f23ee3ccef4fe6
Nick Daniele <ngdaniele@gmail.com>
2019-11-11 15:42:51 -0800

    Configured common webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,1 +1,48 @@
-module.exports = {};
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '__proto__'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  },
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,60 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist'
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: '__proto__'
-    })
-  ],
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
-      },
-      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ['csv-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
       }
     ]
-  },
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
   }
 });
