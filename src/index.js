d8ff9a74f9d20c12c0cb0b811e6ff79446610195
Nick Daniele <ngdaniele@gmail.com>
2019-11-10 18:33:30 -0800

    Changed files based on new webpack bundling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 (function starter() {
-  const root = document.getElementById('root');
-
   const greeting = document.createElement('p');
-
   greeting.innerText = 'Hello World!';
 
-  root.append(greeting);
+  document.body.append(greeting);
 })();
