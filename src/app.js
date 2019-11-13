b93fe37331a750e2c46bdc13c37bbcbc091c8c7c
Nick Daniele <ngdaniele@gmail.com>
2019-11-12 21:00:21 -0800

    Removed shadow dom for css inheritance

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import { LitElement, html } from 'lit-element';
 
 class App extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+
   render() {
     return html`
       <p>Hello World!</p>
