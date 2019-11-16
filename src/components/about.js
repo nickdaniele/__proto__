1d299be8a3272318b57d6974779702aafed9c784
Nick Daniele <ngdaniele@gmail.com>
2019-11-16 14:46:45 -0800

    Removed test content from sections

--- src/components/about.js
+++ src/components/about.js
@@ -18,29 +18,7 @@ class About extends LitElement {
 			<div class="p-2">About</div>
 			${selected
 				? html`
-						<div class="px-2">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-							odio justo, condimentum sed auctor et, auctor a ligula. Duis leo
-							diam, maximus ac pulvinar in, tristique non est. Fusce tortor
-							arcu, bibendum id diam in, hendrerit tincidunt nisl. Maecenas quis
-							fringilla tortor. Nulla suscipit cursus condimentum. Nullam
-							iaculis dignissim dolor nec efficitur. Sed vel nisl nec nisi
-							molestie semper. Donec euismod a massa ut volutpat. Morbi accumsan
-							ultrices porta. Quisque auctor, leo et iaculis blandit, mauris ex
-							maximus purus, quis aliquam arcu justo ac leo. Proin tempus, felis
-							ut sollicitudin sodales, augue metus interdum urna, vitae
-							efficitur lacus eros vel urna. Praesent tempus ligula sit amet
-							neque sollicitudin porttitor. Vivamus placerat dui id venenatis
-							accumsan. Fusce dictum facilisis urna non rhoncus. Aenean in odio
-							lectus. Donec eu magna ac dolor sollicitudin tempus. Ut mollis
-							pellentesque fermentum. Vestibulum ante ipsum primis in faucibus
-							orci luctus et ultrices posuere cubilia Curae; Sed tristique metus
-							in felis dignissim, a pulvinar sapien tristique. Vivamus nec ante
-							a eros malesuada mollis non at mi. Orci varius natoque penatibus
-							et magnis dis parturient montes, nascetur ridiculus mus. Mauris
-							consectetur scelerisque leo, eget posuere enim commodo quis. Sed
-							eleifend porta bibendum.
-						</div>
+						<div class="px-2"></div>
 				  `
 				: null}
 		`;
