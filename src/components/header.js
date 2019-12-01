085f08690938733070276fe16d84a6d1e3cc63d3
Nick Daniele <ngdaniele@gmail.com>
2019-12-01 00:51:51 -0800

    Added handler comment

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,29 @@
 import { LitElement, html } from 'lit-element';
 
 class Header extends LitElement {
-	sectionReset() {
-		this.dispatchEvent(
-			new CustomEvent('section-select', {
-				detail: { section: 'none' }
-			})
-		);
-	}
+  // Events Handlers //////////////////
+  sectionReset() {
+    this.dispatchEvent(
+      new CustomEvent('section-select', {
+        detail: { section: 'none' }
+      })
+    );
+  }
+  /////////////////////////////////////
 
-	createRenderRoot() {
-		return this;
-	}
+  createRenderRoot() {
+    return this;
+  }
 
-	render() {
-		return html`
-			<header class="py-4 text-center border-b border-solid border-black">
-				<h1 class="inline-block cursor-pointer" @click="${this.sectionReset}">
-					__proto__
-				</h1>
-			</header>
-		`;
-	}
+  render() {
+    return html`
+      <header class="py-4 text-center border-b border-solid border-black">
+        <h1 class="inline-block cursor-pointer" @click="${this.sectionReset}">
+          __proto__
+        </h1>
+      </header>
+    `;
+  }
 }
 
 customElements.define('app-header', Header);
